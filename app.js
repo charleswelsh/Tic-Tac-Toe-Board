edf09f69b13faab18f9d5b1cc66eb903ac76aa75
Charles Welsh <charleswelsh@gmail.com>
2020-05-18 11:55:39 -0500

    added reset button and functionality

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-let cells = document.querySelectorAll(`.row >div`);
+const cells = document.querySelectorAll(`.row >div`);
 
 let winningText = document.querySelector(`h2`);
 
+const resetButton = document.getElementById("reset-button");
+
 let playerTurn = "playerOne";
 
-let btnDiv = document.getElementById('buttonDiv')
+let turnCount = 0;
+
+resetButton.addEventListener("click", resetGame);
 
 for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', onClick);
@@ -13,74 +17,60 @@ for (let i = 0; i < cells.length; i++) {
 function checkWin() {
     if (cells[0].textContent + cells[1].textContent + cells[2].textContent === "XXX") {
         winningText.textContent = "X WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[3].textContent + cells[4].textContent + cells[5].textContent === "XXX") {
         winningText.textContent = "X WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[6].textContent + cells[7].textContent + cells[8].textContent === "XXX") {
         winningText.textContent = "X WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[0].textContent + cells[3].textContent + cells[6].textContent === "XXX") {
         winningText.textContent = "X WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[1].textContent + cells[4].textContent + cells[7].textContent === "XXX") {
         winningText.textContent = "X WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[2].textContent + cells[5].textContent + cells[8].textContent === "XXX") {
         winningText.textContent = "X WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[0].textContent + cells[4].textContent + cells[8].textContent === "XXX") {
         winningText.textContent = "X WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[2].textContent + cells[4].textContent + cells[6].textContent === "XXX") {
         winningText.textContent = "X WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[0].textContent + cells[1].textContent + cells[2].textContent === "OOO") {
         winningText.textContent = "O WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[3].textContent + cells[4].textContent + cells[5].textContent === "OOO") {
         winningText.textContent = "O WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[6].textContent + cells[7].textContent + cells[8].textContent === "OOO") {
         winningText.textContent = "O WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[0].textContent + cells[3].textContent + cells[6].textContent === "OOO") {
         winningText.textContent = "O WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[1].textContent + cells[4].textContent + cells[7].textContent === "OOO") {
         winningText.textContent = "O WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[2].textContent + cells[5].textContent + cells[8].textContent === "OOO") {
         winningText.textContent = "O WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[0].textContent + cells[4].textContent + cells[8].textContent === "OOO") {
         winningText.textContent = "O WINS!!!";
-        showButton()
+       resetButton.style.display = "block";
     } else if (cells[2].textContent + cells[4].textContent + cells[6].textContent === "OOO") {
         winningText.textContent = "O WINS!!!";
-        showButton()
-    };
+       resetButton.style.display = "block";
+    } else if (turnCount === 9) {
+        winningText.textContent = "DRAW";
+        resetButton.style.display = "block";
+    }
 };
 
-function showButton() {
-    let btn = document.createElement('button')
-    btn.className += 'restart-btn'
-    btnDiv.appendChild(btn)
-    btn.innerText = 'Rematch?'
-    btn.addEventListener('click', function () {
-        restart()
-    })
-}
-
-function restart() {
-    cells.forEach(cell => {
-        cell.textContent = ''
-        playerTurn = 'playerOne'
-        winningText.textContent = ''
-    })
-}
-
 function onClick() {
+    turnCount++
     if ((event.target.textContent === "") && (playerTurn === "playerOne")) {
         event.target.textContent = "X";
         playerTurn = "playerTwo";
@@ -90,4 +80,14 @@ function onClick() {
         playerTurn = "playerOne";
         checkWin();
     }
+};
+
+function resetGame() {
+    turnCount = 0;
+    playerTurn = "player1";
+    cells.forEach(function (cell) {
+        cell.textContent = "";    
+    })
+    winningText.textContent = "";
+    resetButton.style.display  = "none";
 };
